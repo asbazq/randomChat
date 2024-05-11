b9105fd2002e91d8998e2fa37d7da166fba0d47e
asbazq <wjdgns5488@naver.com>
2024-05-12 00:52:49 +0900

    feat: security + jwt + redis 고도화

--- src/main/java/com/example/stompTest/security/WebSocketConfig.java
+++ src/main/java/com/example/stompTest/security/WebSocketConfig.java
@@ -1,12 +1,8 @@
 package com.example.stompTest.security;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
-=======
-import org.springframework.context.annotation.Configuration;
->>>>>>> a9d529bb42d525b8b869238cddc213d7e57b123f
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,7 +12,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-<<<<<<< HEAD
     private final StompHandler stompHandler;
 
     @Autowired
@@ -28,12 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
         registry.enableSimpleBroker("/sub"); // 클라이언트에서 보낸 메세지를 받을 prefix
-=======
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-        registry.enableSimpleBroker("/sub");
->>>>>>> a9d529bb42d525b8b869238cddc213d7e57b123f
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
@@ -43,11 +32,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
     }
     
-<<<<<<< HEAD
      @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(stompHandler);
     }
-=======
->>>>>>> a9d529bb42d525b8b869238cddc213d7e57b123f
 }
